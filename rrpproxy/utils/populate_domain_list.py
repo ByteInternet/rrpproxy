46ccd99b95acf9e03c86c9f7f37840b588ef6d2a
alejandro <alejandrogarz@gmail.com>
2021-12-16 14:47:07 +0100

    add docstring to populate_domain_list

--- rrpproxy/utils/populate_domain_list.py
+++ rrpproxy/utils/populate_domain_list.py
@@ -1,5 +1,15 @@
 
 def populate_domain_list(domains_in_rrpproxy_format):
+    """
+    This method takes the default format of RRP domains
+    (a dictionary of lists for each property) and converts it
+    to a list of dictionaries. Each dict in the list represents
+    a domain and has the various properties as keys.
+    A property will be absent in the dict if it has it had no value.
+    :param dict domains_in_rrpproxy_format: Dict of lists with domain properties
+    :return: List of domain dictionaries
+    :rtype: list
+    """
     domain_list = []
     properties = domains_in_rrpproxy_format['property']
     for index in range(len(properties['domain'])):
